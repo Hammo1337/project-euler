d2c37f8781a1f59ee9fab21c4d0edd68cf99a847
Richard Hammans <richard.hammans@mnscorp.net>
2018-04-20 18:18:16 +0100

    Added solution 2 and 3, 3 still broken | working answer, not in current structure

--- src/EulerProject.java
+++ src/EulerProject.java
@@ -4,6 +4,7 @@ import utilities.*;
 public class EulerProject {
     public static void main(String[] args) {
         Utility util = new Utility();
+
         int n;
         boolean done = false;
 
@@ -15,14 +16,14 @@ public class EulerProject {
                     Solution1 solution1 = new Solution1();
                     solution1.Solve();
                     break;
-//                case 2:
-//                    Solution2 solution2 = new Solution2();
-//                    solution2.Solve();
-//                    break;
-//                case 3:
-//                    Solution3 solution3 = new Solution3();
-//                    solution3.Solve();
-//                    break;
+                case 2:
+                    Solution2 solution2 = new Solution2();
+                    solution2.Solve();
+                    break;
+                case 3:
+                    Solution3 solution3 = new Solution3();
+                    solution3.Solve();
+                    break;
                   case 999:
                       done = true;
                       break;
